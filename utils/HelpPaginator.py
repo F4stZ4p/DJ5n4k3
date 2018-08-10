649a29ddf70b188a4550e2150c3ddc4dac4ec889
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-08-11 00:27:00 +0300

    Update HelpPaginator.py

--- utils/HelpPaginator.py
+++ utils/HelpPaginator.py
@@ -47,7 +47,7 @@ class Pages:
         if left_over:
             pages += 1
         self.maximum_pages = pages
-        self.embed = discord.Embed(colour=0xff0000) # any HEX color here
+        self.embed = discord.Embed(colour=0xf4df42) # any HEX color here
         self.paginating = len(entries) > per_page
         self.show_entry_count = show_entry_count
         self.reaction_emojis = [
