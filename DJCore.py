4e4dce691e003207d8dc2837131f2a62302552cd
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-08-12 23:37:11 +0300

    Update DJCore.py

--- DJCore.py
+++ DJCore.py
@@ -14,7 +14,7 @@ async def _bot_protection(ctx):
 @bot.event
 async def on_ready():
     print(f'Bot is ready! Logged as in: {bot.user}')
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="your moves | ;;;help"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="your move | ;;;help"))
 
 if __name__ == "__main__":
     for extension in bot.initials:
